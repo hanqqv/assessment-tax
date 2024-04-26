2d8d320fbdef68ab1cd91ce1140e821deffedcce
vitsarut <han_f50@hotmail.com>
2024-04-26 18:08:38 +0700

    add math round in calculateTax

--- postgres/tax.go
+++ postgres/tax.go
@@ -1,6 +1,8 @@
 package postgres
 
 import (
+	"math"
+
 	"github.com/hanqqv/assessment-tax/tax"
 )
 
@@ -11,23 +13,28 @@ func (p *Postgres) CalculateTax(userInfo tax.UserInfo) (tax.Tax, error) {
 
 	if netAmount <= 150000.0 {
 		taxAmount = 0.0
+		taxAmount = math.Round(taxAmount*100) / 100
 		return tax.Tax{Tax: taxAmount}, nil
 	}
 
 	if netAmount <= 500000.0 {
 		taxAmount = 0.10 * (netAmount - 150000.0)
+		taxAmount = math.Round(taxAmount*100) / 100
 		return tax.Tax{Tax: taxAmount}, nil
 	}
 
 	if netAmount <= 1000000.0 {
 		taxAmount = 35000.0 + (0.15 * (netAmount - 500000.0))
+		taxAmount = math.Round(taxAmount*100) / 100
 		return tax.Tax{Tax: taxAmount}, nil
 	}
 
 	if netAmount <= 2000000.0 {
 		taxAmount = 35000.0 + 75000.0 + (0.20 * (netAmount - 1000000.0))
+		taxAmount = math.Round(taxAmount*100) / 100
 		return tax.Tax{Tax: taxAmount}, nil
 	}
 	taxAmount = 35000.0 + 75000.0 + 200000.0 + (0.35 * (netAmount - 2000000.0))
+	taxAmount = math.Round(taxAmount*100) / 100
 	return tax.Tax{Tax: taxAmount}, nil
 }
