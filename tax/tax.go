c43fd79f13c395e728bcc7a3719dea5b99b20a12
vitsarut <han_f50@hotmail.com>
2024-04-26 20:07:09 +0700

    add TaxLevel struct

--- tax/tax.go
+++ tax/tax.go
@@ -12,6 +12,13 @@ type Allowances struct {
 }
 
 type Tax struct {
+	Tax       float64    `json:"tax"`
+	TaxRefund float64    `json:"taxRefund,omitempty"`
+	TaxLevel  []TaxLevel `json:"taxLevel"`
+}
+
+type TaxLevel struct {
+	Level     string  `json:"level"`
 	Tax       float64 `json:"tax"`
 	TaxRefund float64 `json:"taxRefund,omitempty"`
 }
